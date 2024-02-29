2b2c739f9096ac4287ad508e36554495fe3e6ad5
ELS <57393100+kodosa7@users.noreply.github.com>
2024-02-29 08:31:24 +0100

    Fix of element bubbling

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const navToggle = document.querySelector(".nav__toggle");
-const navLinks = document.querySelectorAll(".nav__link");
+const navLinks = document.querySelector(".nav__list");
 
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
+navLinks.addEventListener("click", (e) => {
+    if (e.target.matches(".nav__item")) {
         document.body.classList.remove("nav-open");
-    });
+    }
 });
